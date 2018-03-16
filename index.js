6513aece1ff633c247e0e903c623e6bc2cb62ecc
mrsrilanka <30635560+mrsrilanka@users.noreply.github.com>
2018-03-16 14:25:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Welcome to Datera K8s test cluster with Docker!');
 });
 
 var server = app.listen(3000, function () {
